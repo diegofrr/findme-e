2873400bb03033269d7a4512b81f6101372108cb
Diêgo Ferreira <56923620+diegofrr@users.noreply.github.com>
2022-04-30 14:39:52 -0300

    Update script.js

--- memory_game/js/script.js
+++ memory_game/js/script.js
@@ -1,15 +1,51 @@
 let btn = document.querySelector('#ff');
 const cardsContainer = document.querySelectorAll('.cards-container .card');
-let selectedCards = []
-let checkedCards = []
-let imagesIdentifiers = shuffleArray(1, 20)
-console.log(imagesIdentifiers)
+let selectedCards = [];
+let checkedCards = [];
+let imagesIdentifiers = shuffleArray(1, 11);
+let statusTempo = document.querySelector('.status__tempo span');
+let statusJogadas = document.querySelector('.status__jogadas span');
+let containerGeral = document.querySelector('.container');
+let loadingIcon = document.querySelector('.loadingIcon');
+// buttons
+let restartGameBtn = document.querySelector('.restartGameBtn');
+let playGameBtn = document.querySelector('.playGame');
+let finalStatus = document.querySelector('.final-status');
+// counters
+let chkCardsQtd = 0;
+let actualTime = 0;
+let attPlays = 0;
+let socialMenuBtn = document.querySelector('.social-btn');
+let socialMenu = document.querySelector('.social-menu');
+let openMenu = document.querySelector('._open');
+let closeMenuBtn = document.querySelector('._close');
+let bgCloseMenu = document.querySelector('#bg-to-close-menu');
+
+socialMenuBtn.addEventListener('click', () => {
+    closeOpenMenu();
+})
+
+bgCloseMenu.addEventListener('click', () => {
+    closeOpenMenu();
+})
+
+function closeOpenMenu() {
+    socialMenu.classList.toggle('toggle-menu');
+    openMenu.classList.toggle('_display-none');
+    closeMenuBtn.classList.toggle('_display-none');
+    closeMenuBtn.classList.toggle('close--active')
+    bgCloseMenu.classList.toggle('_display-none')
+}
+
+
 
 function shuffleArray(start, end) {
     let arr = []
-    for (k = start; k < end + 1; k++) {
+    for (k = start; k < end; k++) {
         arr.push(k);
+        arr.push(k)
     }
+
     for (let i = arr.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [arr[i], arr[j]] = [arr[j], arr[i]];
@@ -19,8 +55,10 @@ function shuffleArray(start, end) {
 
 cardsContainer.forEach((card) => {
     let id = imagesIdentifiers.pop();
-    let src = `http://picsum.photos/id/${id}/100/130`
-    card.lastElementChild.getAttribute('src') = src;
+    src = `
+    https://raw.githubusercontent.com/diegofrr/findme.e/main/memory_game/images/cards-image/image${id}.png
+    `
+    card.lastElementChild.src = src;
 })
 
 
@@ -43,6 +81,7 @@ function enable() {
     allCardsPointerEvents('all')
 }
 
+
 cardsContainer.forEach((card) => {
     card.addEventListener('click', (e) => {
         e.target.style.pointerEvents = 'none';
@@ -58,6 +97,7 @@ cardsContainer.forEach((card) => {
         selectedCards.push(e);
 
         if (selectedCards.length === 2) {
+            addJogadas();
             allCardsPointerEvents('none');
             compareCards(selectedCards);
         }
@@ -76,7 +116,6 @@ function compareCards(selectedCardsToCompare) {
     if (card1ImgLink === card2ImgLink) {
         equalsCards(card1, card2);
     } else {
-        console.log('diferente')
         setTimeout(() => {
             enable();
         }, 1000);
@@ -84,11 +123,30 @@ function compareCards(selectedCardsToCompare) {
 
 }
 
-
 function allCardsPointerEvents(state) {
-    cardsContainer.forEach((card) => {
-        card.firstElementChild.style.pointerEvents = state;
+    document.querySelectorAll('.cards-container .card .card__stamp').forEach((span) => {
+        span.style.pointerEvents = state;
+        span.style.filter = 'none';
     })
+}
+
+function addChkIcon(cards) {
+    cards.forEach((card) => {
+        card.path[1].innerHTML =
+            `<span id="nice"><i class="uil uil-check"></i></span>` +
+            card.path[1].innerHTML
+    })
+
+    cards.forEach((card) => {
+        card.path[1].firstElementChild.firstElementChild.classList.add('_upup');
+    })
+
+
+    setTimeout(() => {
+        document.querySelectorAll('#nice i').forEach((e) => {
+            e.classList.remove('_upup');
+        })
+    }, 1000);
 
 }
 
@@ -98,9 +156,105 @@ function equalsCards(card1, card2) {
     checkedCards.push(card1);
     checkedCards.push(card2);
 
+    chkCardsQtd++;
+
+    if (chkCardsQtd === 10) {
+        setTimeout(() => {
+            finalResult(attPlays, actualTime);
+        }, 1000);
+    }
+
     setTimeout(() => {
         allCardsPointerEvents('all');
         selectedCards = []
     }, 2000);
 
+    setTimeout(() => {
+        addChkIcon([card1, card2]);
+    }, 1000);
+
+}
+
+
+// status code
+
+function addJogadas() {
+    attPlays++;
+    if (statusJogadas.textContent < 9) {
+        let n = statusJogadas.textContent;
+        n++;
+        statusJogadas.textContent = '0' + n;
+        return;
+    }
+    statusJogadas.textContent++;
+
+}
+
+playGameBtn.addEventListener('click', () => {
+    allCardsPointerEvents('all')
+    let seconds = 0;
+    let minutes = 0;
+    setInterval(() => {
+        seconds++;
+
+        if (seconds > 59) {
+            minutes++;
+            seconds = 0;
+        }
+
+        if (seconds < 10) {
+            statusTempo.innerHTML = `${minutes}:0${seconds}`;
+            actualTime = `${minutes}:0${seconds}`;
+        } else {
+            statusTempo.innerHTML = `${minutes}:${seconds}`;
+            actualTime = `${minutes}:${seconds}`;
+        }
+
+    }, 1000);
+    playGameBtn.style.display = 'none';
+    restartGameBtn.style.display = 'inline';
+})
+
+restartGameBtn.addEventListener('click', () => {
+    location.reload();
+})
+
+containerGeral.style.display = 'none';
+
+// Icon de carregamento
+setInterval(() => {
+    containerGeral.style.display = 'block';
+    loadingIcon.style.display = 'none';
+}, 2000);
+
+
+// RESULTADO FINAL
+
+function finalResult(att, time) {
+    finalStatus.style.display = 'grid';
+    finalStatus.innerHTML = (
+        `
+    <div class="f-container">
+                <img class="f-container__image" src="https://raw.githubusercontent.com/diegofrr/findme.e/main/memory_game/images/f-emoji.png">
+
+                <p class="f-msg">Parabéns, você encontrou todos os pares de cartas!!!</p>
+                <p class="f-statics">Aqui estão suas estatísticas:</p>
+                <p class="f-tentativas">Tentativas >
+                    <span>${att}</span>
+                </p>
+
+                <p class="f-tempo">
+                    Tempo >
+                    <span>${time}</span>
+                </p>
+
+
+                <button class="f-btn">Quero jogar de novo!</button>
+            </div>
+    `);
+    document.querySelector('.f-btn').addEventListener('click', () => {
+        location.reload();
+    })
+
+    document.querySelector('.status-bar').style.display = 'none';
 }
